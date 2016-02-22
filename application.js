2e9b3b30b43c6ba5345f0bfa525e40469bb68c39
Zach Schatz <zachschatz@gmail.com>
2016-02-22 15:44:34 -0500

    Remove comments

--- application.js
+++ application.js
@@ -1,20 +1,12 @@
 $(document).ready(function(){
   $("nav").on('click', 'a', function(event) {
-
-    // Prevent default anchor click behavior
     event.preventDefault();
 
-    // Store hash
     var hash = this.hash;
 
-    // Using jQuery's animate() method to add smooth page scroll
-    // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
     $('html, body').animate({
       scrollTop: $(hash).offset().top
     }, 700, function(){
-
-      // Add hash (#) to URL when done scrolling (default click behavior)
-      // window.location.hash = hash;
     });
   });
 });
